0b2a44c2e7b116fd9bebae54ca46f933caef8aa6
MatthiasBenaets <matthias.benaets@gmail.com>
2026-09-08 00:06:29 +0200

    feat: aerospace

---
+++ Casks/aerospace-bsp.rb
@@ -0,0 +1,23 @@
+cask "aerospace-bsp" do
+  version "0.0.1-bsp"
+  sha256 "74239715e927340248b55bd21ee20d38a51fef98c02514ea7f59e9a54f35217f"
+  url "https://github.com/MatthiasBenaets/AeroSpace/releases/download/v#{version}/AeroSpace-v#{version}.zip"
+  name "AeroSpace"
+  desc "AeroSpace is an i3-like tiling window manager for macOS (BSP fork)"
+  homepage "https://github.com/MatthiasBenaets/AeroSpace"
+  depends_on macos: :ventura
+  conflicts_with cask: "aerospace"
+  postflight do
+    system "xattr", "-d", "com.apple.quarantine", "#{staged_path}/AeroSpace-v#{version}/bin/aerospace"
+    system "xattr", "-d", "com.apple.quarantine", "#{appdir}/AeroSpace.app"
+  end
+  app "AeroSpace-v#{version}/AeroSpace.app"
+  binary "AeroSpace-v#{version}/bin/aerospace"
+  uninstall quit: "bobko.aerospace"
+  caveats <<~EOS
+    To start AeroSpace at login:
+      1. Enable "start-at-login = true" in ~/.aerospace.toml
+    OR
+      2. Add AeroSpace.app to Login Items in System Settings
+  EOS
+end
